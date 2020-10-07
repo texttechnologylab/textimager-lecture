8758b6745885b4dcbeb758341c5ba52118d27550
Daniel Baumartz <baumartz@stud.uni-frankfurt.de>
2020-10-07 14:35:07 +0200

    model

--- textimager-uima-textblob/src/main/resources/textblob_0.15.3_v2_setup.sh
+++ textimager-uima-textblob/src/main/resources/textblob_0.15.3_v2_setup.sh
@@ -26,19 +26,19 @@ if [ ! -e $MODELS_DOWNLOADED ]; then
 	echo "downloading models..."
 	python3 -m spacy download de_core_news_sm
 	python3 -m spacy download en_core_web_sm
-	python3 -m spacy download zh_core_web_sm
-	python3 -m spacy download da_core_news_sm
-	python3 -m spacy download nl_core_news_sm
-	python3 -m spacy download fr_core_news_sm
-	python3 -m spacy download el_core_news_sm
-	python3 -m spacy download it_core_news_sm
-	python3 -m spacy download ja_core_news_sm
-	python3 -m spacy download lt_core_news_sm
-	python3 -m spacy download nb_core_news_sm
-	python3 -m spacy download pl_core_news_sm
-	python3 -m spacy download pt_core_news_sm
-	python3 -m spacy download ro_core_news_sm
-	python3 -m spacy download es_core_news_sm
+	#python3 -m spacy download zh_core_web_sm
+	#python3 -m spacy download da_core_news_sm
+	#python3 -m spacy download nl_core_news_sm
+	#python3 -m spacy download fr_core_news_sm
+	#python3 -m spacy download el_core_news_sm
+	#python3 -m spacy download it_core_news_sm
+	#python3 -m spacy download ja_core_news_sm
+	#python3 -m spacy download lt_core_news_sm
+	#python3 -m spacy download nb_core_news_sm
+	#python3 -m spacy download pl_core_news_sm
+	#python3 -m spacy download pt_core_news_sm
+	#python3 -m spacy download ro_core_news_sm
+	#python3 -m spacy download es_core_news_sm
 else
 	echo "using predownloaded models"
 	pip install /resources/nlp/models/spacy/de_core_news_sm-2.3.0.tar.gz
